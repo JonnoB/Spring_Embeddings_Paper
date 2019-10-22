5159e38f54d79e41cc6cf790f313d7445e08f5df
Jonathan Bourne <cheg_a_leg@hotmail.com>
2019-10-22 18:33:25 +0100

    Simple code
    I needed to simplify the script which was getting out of hand. I created Spring_Embeddings_Data_Generation that does all the work necessary for the HPC script to work. I also changed the HPC script so that it runs multiple simulations at a time.

--- HPC_Spring_Embeddings_script.R
+++ HPC_Spring_Embeddings_script.R
@@ -39,7 +39,6 @@ if(dir.exists("/home/jonno")){
   basewd <- "~/Dropbox"
 }
 
-IEEE_networks <- file.path(Project_folder, "IEEE_network_files")
 
 #Load some other useful functions
 list.files(file.path(basewd, "Useful_PhD__R_Functions"), pattern = ".R", full.names = T) %>%
@@ -49,70 +48,101 @@ list.files(file.path(basewd, "Flow_Spring_System"), pattern = ".R", full.names =
   walk(~source(.x))
 
 
-##
-##
-#only loads IEEE_118 and the wrong one at that! delete and replace with a parametrised version!
-##
-##
-g <- readRDS(file = graph_path) #read the target graph
+parameter_df_temp <- readRDS(file.path(Project_folder, "parameter_file.rds")) %>%
+  filter(compute_group ==computation_number) #this variable is inserted into the file
 
 
-#Proportionally load the network
-g <- Proportional_Load(g, alpha = ec)
+1:nrow(parameter_df_temp) %>%
+  walk(~{
+    
+    ##
+    ##
+    ##This block below gets the variables necessary to perform the calculation
+    ##
+    ##
+    Iter <- parameter_df_temp %>%
+      slice(.x)
+    
+    scramble_network <- Iter$scramble_network #Needs to be added into the target orders file
+    graph_path <- Iter$graph_path
+    scramble_seed <- Iter$seed
+    ec <- Iter$ec
+    v <- Iter$v
+    fract <- Iter$fract
+    permutation <- Iter$permutation
+    deletion_seed <- Iter$deletion_seed
+    attack_id <- Iter$Simulation_ID
+    Iter_collapse_path <- Iter$collapse_path
+    Iter_collapse_summary_path <- Iter$collapse_summary_path
+    ##
+    ##
+    ##Once the variables have been taken from the parameter file the calculation can begin
+    ##
+    ##
+    
+    
+    g <- readRDS(file = graph_path) #read the target graph
+    
+    
+    #Proportionally load the network
+    g <- Proportional_Load(g, alpha = ec)
+    
+    #For most of the simulations we will scramble the proportionally loaded networks,
+    #However when we are calculating the limits of 1 and Inf as well as the proportional line for the test cases
+    #scrambling is not required. As a result these lines of code make the overall script more flexible.
+    if(scramble_network){
+      #scramble the excess capaacity
+      edge_order_df <- Create_scrambled_edges(g, scramble_seed, fract = fract)
+      
+      g <- g %>%
+        set.edge.attribute(., "Link.Limit", value = edge_order_df$Link.Limit)
+      
+    }
+    
+    #Set the seed for the random order and generate the edge deletion order
+    set.seed(deletion_seed)
+    DeletionOrder <- RandomAttack(g, Target = "Edges", Number = ecount(g), Name = "Link")
+    #Construction the deletion order function.
+    FixedNodes <- quo(FixedStrategyAttack(g, DeletionOrder, "Edge", Name =  "Link"))
+    
+    AttackSeries <- suppressMessages(AttackTheGrid(NetworkList = list(list(g)),
+                                                   AttackStrategy = FixedNodes,
+                                                   referenceGrid = NULL,
+                                                   MinMaxComp = 0.0,
+                                                   TotalAttackRounds=1000,
+                                                   CascadeMode = TRUE,
+                                                   CumulativeAttacks = NULL,
+                                                   Demand = "Demand",
+                                                   Generation = "Generation",
+                                                   EdgeName = "Link",
+                                                   VertexName = "name",
+                                                   Net_generation = "Net_Generation"))
+    
+    
+    #This is because doing all the extraction at the end would take a very long time, so I break it up over all the jobs.
+    #This way I only need to combine the saved files but not operate on them.
+    AttackSeriesSummary <- AttackSeries %>%
+      ExtractNetworkStats(Generation = "Generation", EdgeName = "Link", PowerFlow = "PowerFlow", Link.Limit = "Link.Limit") %>%
+      mutate(attack_id = attack_id,
+             GridLoading = ifelse(Blackout==1, 0, GridLoading))
+    
+    #Both the attack series and the summary are saved to store as much information as possible in one go.
+    
+    #The structure is generated as needed and so any new paths can just be created at this point.
+    #There is very little overhead in doing it this way
+    c(dirname(Iter_collapse_path), dirname(Iter_collapse_summary_path) ) %>% walk(~{
+      if(!file.exists(.x)) dir.create(.x, recursive = T)
+    })
+    
+    saveRDS(AttackSeries, file = Iter_collapse_path)
+    
+    saveRDS(AttackSeriesSummary, file = Iter_collapse_summary_path)
+    #After the simulation and its summary are saved to the drive the next in the compute group is calculated
+    
+  })
 
-#For most of the simulations we will scramble the proportionally loaded networks,
-#However when we are calculating the limits of 1 and Inf as well as the proportional line for the test cases
-#scrambling is not required. As a result these lines of code make the overall script more flexible.
-if(scramble_network){
-#scramble the excess capaacity
-edge_order_df <- Create_scrambled_edges(g, scramble_seed, fract = fract)
 
-g <- g %>%
-  set.edge.attribute(., "Link.Limit", value = edge_order_df$Link.Limit)
-
-}
-
-#Set the seed for the random order and generate the edge deletion order
-set.seed(deletion_seed)
-DeletionOrder <- RandomAttack(g, Target = "Edges", Number = ecount(g), Name = "Link")
-#Construction the deletion order function.
-FixedNodes <- quo(FixedStrategyAttack(g, DeletionOrder, "Edge", Name =  "Link"))
-
-AttackSeries <- suppressMessages(AttackTheGrid(NetworkList = list(list(g)),
-                              AttackStrategy = FixedNodes,
-                              referenceGrid = NULL,
-                              MinMaxComp = 0.0,
-                              TotalAttackRounds=1000,
-                              CascadeMode = TRUE,
-                              CumulativeAttacks = NULL,
-                              Demand = "Load_MW",
-                              Generation = "Generation_MW",
-                              EdgeName = "Link",
-                              VertexName = "name",
-                              Net_generation = "Net_Generation"))
-
-
-#This is becuase doing all the extraction at the end would take a very long time, so I break it up over all the jobs.
-#This way I only need to combine the saved files but not operate on them.
-AttackSeriesSummary <- AttackSeries %>%
-  ExtractNetworkStats(Generation = "Generation_MW", EdgeName = "Link", PowerFlow = "PowerFlow", Link.Limit = "Link.Limit") %>%
-  mutate(attack_id = attack_id,
-         GridLoading = ifelse(Blackout==1, 0, GridLoading))
-
-#Both the attack series and the summary are saved to store as much information as possible in one go.
-
-#The structure is generated as needed and so any new paths can just be created at this point.
-#There is very little overhead in doing it this way
-c(dirname(Iter_collapse_path), dirname(Iter_collapse_summary_path) ) %>% walk(~{
-  if(!file.exists(.x)) dir.create(.x, recursive = T)
-})
-
-saveRDS(AttackSeries, file = Iter_collapse_path)
-
-saveRDS(AttackSeriesSummary, file = Iter_collapse_summary_path)
-
-
-#After the simulation and its summary are saved to the drive the script is complete
+#Once all the simulations in the compute group have been saved the script is complete
 
 #######################
 #######################
